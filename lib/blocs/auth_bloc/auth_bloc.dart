a32c7a81ebf257421524cefa6b001269a2fec4d3
Ahmad Ruslan <aruslan8395@gmail.com>
2020-07-03 14:27:36 +0700

    [ADD]ADD auth bloc to auth bloc repo

---
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:passion_jewelry/utils/user_repository.dart';
+
+import 'auth_event.dart';
+import 'auth_state.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+  AuthenticationBloc({@required this.userRepository}): assert(userRepository != null);
+
+  @override
+  AuthenticationState get initialState => AuthenticationInitial();
+
+  @override
+Stream<AuthenticationState> mapEventToState(
+  AuthenticationEvent event,
+) async* {
+  if (event is AuthenticationStarted) {
+    final bool hasToken = await userRepository.hasToken();
+
+    if (hasToken) {
+      yield AuthenticationSuccess();
+    } else {
+      yield AuthenticationFailure();
+    }
+  }
+
+  if (event is AuthenticationLoggedIn) {
+    yield AuthenticationInProgress();
+    await userRepository.persistToken(event.token);
+    yield AuthenticationSuccess();
+  }
+
+  if (event is AuthenticationLoggedOut) {
+    yield AuthenticationInProgress();
+    await userRepository.deleteToken();
+    yield AuthenticationFailure();
+  }
+}
+
+}
